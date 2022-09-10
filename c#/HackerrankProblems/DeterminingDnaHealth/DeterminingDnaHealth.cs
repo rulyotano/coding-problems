7664bba92bf92bc3f29b597058b684bc536379ff
Raúl Otaño Hurtado <rulyotano@gmail.com>
2022-09-10 21:49:12 +0200

    fix types to ulong

--- c#/HackerrankProblems/DeterminingDnaHealth/DeterminingDnaHealth.cs
+++ c#/HackerrankProblems/DeterminingDnaHealth/DeterminingDnaHealth.cs
@@ -9,15 +9,14 @@ public class DeterminingDnaHealth
     Gene[] genes = Console.ReadLine().TrimEnd().Split(' ').Select((g, i) => new Gene { Value = g, Index = i }).ToArray();
     Console.ReadLine().TrimEnd().Split(' ').Select((healthTemp, i) =>
     {
-      genes[i].Health = Convert.ToInt32(healthTemp);
-      genes[i].AccumulativeHealth = (i > 0 ? genes[i - 1].AccumulativeHealth : 0) + genes[i].Health;
+      genes[i].Health = Convert.ToUInt32(healthTemp);
       return 1;
     }).ToList();
 
     int s = Convert.ToInt32(Console.ReadLine().Trim());
 
-    var maxHealthSum = 0L;
-    var minHealthSum = long.MaxValue;
+    var maxHealthSum = 0UL;
+    var minHealthSum = ulong.MaxValue;
     for (int sItr = 0; sItr < s; sItr++)
     {
       string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
@@ -30,14 +29,10 @@ public class DeterminingDnaHealth
 
       const int maxPrefix = 4;
       var prefixIndexes = BuildPrefixMap(d, maxPrefix);
-      var currentHealthSum = 0L;
+      var currentHealthSum = 0UL;
 
       foreach (var gene in genes.Skip(first).Take(last - first + 1))
       {
-        if (currentHealthSum >= minHealthSum
-          && genes[last].AccumulativeHealth - genes[first].AccumulativeHealth + genes[first].Health < maxHealthSum)
-          break;
-
         var prefix = gene.GetPrefix(maxPrefix);
         if (prefixIndexes.ContainsKey(prefix))
         {
@@ -109,9 +104,9 @@ public class DeterminingDnaHealth
 
     public string Value { get; set; }
 
-    public int Health { get; set; }
+    public uint Health { get; set; }
 
-    public double AccumulativeHealth { get; set; }
+    public ulong AccumulativeHealth { get; set; }
 
     public string GetPrefix(int maxPrefixSizes) => Value.Substring(0, Math.Min(maxPrefixSizes, Value.Length));
   };
