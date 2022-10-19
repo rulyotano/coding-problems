42d830b6c765fd42424dd7f31a8a0ebb3b5a6837
Raul <rulyotano@gmail.com>
2022-10-19 15:38:49 +0200

    jump game III

---
+++ c#/HackerrankProblems/JumpGameIII/JumpGameIII.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+
+public class JumGameIIISolution
+{
+    public bool CanReach(int[] arr, int start)
+    {
+        var marks = new BitArray(arr.Length);
+        var nextNodes = new Queue<int>();
+
+        nextNodes.Enqueue(start);
+        marks[start] = true;
+
+        while (nextNodes.Count > 0)
+        {
+            var currentIndex = nextNodes.Dequeue();
+            if (arr[currentIndex] == 0) return true;
+            var leftIndex = currentIndex - arr[currentIndex];
+            var rightIndex = currentIndex + arr[currentIndex];
+            if (leftIndex >= 0 && !marks[leftIndex])
+            {
+                nextNodes.Enqueue(leftIndex);
+                marks[leftIndex] = true;
+            }
+            if (rightIndex < arr.Length && !marks[rightIndex])
+            {
+                nextNodes.Enqueue(rightIndex);
+                marks[rightIndex] = true;
+            }
+        }
+        return false;
+    }
+}
---
+++ c#/HackerrankProblems/JumpGameIII/README.md
@@ -0,0 +1,5 @@
+Problem URL: [Jump Game III](https://leetcode.com/problems/jump-game-iii/)
+
+> tags: `#array`, `#dfs`, `#bfs`
+
+1st try took: 15mins
